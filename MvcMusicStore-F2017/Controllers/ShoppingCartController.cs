2738d08b5fe1095cb1146e156306c72c02a26457
ifotn <rich@infrontofthenet.com>
2017-12-05 14:52:50 -0500

    checkout

--- MvcMusicStore-F2017/Controllers/ShoppingCartController.cs
+++ MvcMusicStore-F2017/Controllers/ShoppingCartController.cs
@@ -9,6 +9,9 @@ namespace MvcMusicStore_F2017.Controllers
 {
     public class ShoppingCartController : Controller
     {
+        // db connection
+        private MusicStoreModel db = new MusicStoreModel();
+
         // GET: ShoppingCart
         public ActionResult Index()
         {
@@ -38,6 +41,64 @@ namespace MvcMusicStore_F2017.Controllers
             // redirect to Index which will display the current cart
             return RedirectToAction("Index");
 
+        }
+
+        [Authorize]
+        // GET: Checkout
+        public ActionResult Checkout()
+        {
+            return View();
+        }
+
+        [Authorize]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        // POST: Checkout
+        public ActionResult Checkout(FormCollection values)
+        {
+            // create a new order and populate the fields from the form
+            var order = new Order();
+            TryUpdateModel(order);
+
+            // check the PromoCode for a value of "FREE"
+            if(values["PromoCode"] != "FREE")
+            {
+                ViewBag.Message = "Use Promo Code FREE";
+                return View(order);
+            }
+            else
+            {
+                // Promo Code is good so save the order
+                // auto-fill the username, email, date, and total
+                order.Username = User.Identity.Name;
+                order.Email = User.Identity.Name;
+                order.OrderDate = DateTime.Now;
+
+                var cart = ShoppingCart.GetCart(this.HttpContext);
+                order.Total = cart.GetTotal();
+
+                // save
+                db.Orders.Add(order);
+                db.SaveChanges();
+
+                // save order details from cart
+                var cartItems = cart.GetCartItems();
+
+                foreach(Cart item in cartItems)
+                {
+                    var orderDetail = new OrderDetail();
+                    orderDetail.OrderId = order.OrderId;
+                    orderDetail.AlbumId = item.AlbumId;
+                    orderDetail.Quantity = item.Count;
+                    orderDetail.UnitPrice = item.Album.Price;
+                    db.OrderDetails.Add(orderDetail);
+                }
+
+                db.SaveChanges();
+            }
+
+            // redirect to Order Details
+            return RedirectToAction("Details", "Order", new { Id = order.OrderId });
         }
     }
 }
